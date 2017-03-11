ac0d2d77102c3d2ce3c26b65301b8d3047e7b987
ymcatar <ymcatar@gmail.com>
2017-03-11 21:11:09 +0800

    add: layer 2 key

--- keyboards/xd60/keymaps/ymcatar/keymap.c
+++ keyboards/xd60/keymaps/ymcatar/keymap.c
@@ -17,7 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,    KC_P,    KC_LBRC,  KC_RBRC,           KC_BSPC,   \
       TD(A),   KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,    KC_SCLN, KC_QUOT,  KC_NO,             KC_ENT,    \
       KC_LSPO, KC_NO,   KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH,  KC_RSPC, KC_RSPC,  KC_DEL,    \
-      MO(1),   KC_LALT, KC_LGUI,                          KC_SPC,                          MO(1),   KC_LEFT,  KC_DOWN, KC_UP,    KC_RIGHT),
+      MO(2),   KC_LALT, KC_LGUI,                          KC_SPC,                          MO(1),   KC_LEFT,  KC_DOWN, KC_UP,    KC_RIGHT),
 
   // 1: Function Layer
   KEYMAP(
@@ -33,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_NO,   RGB_TOG, RGB_MOD, KC_NO,   KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,   KC_NO,   KC_NO,    KC_NO,             KC_NO,    \
       KC_NO,   RGB_HUI, RGB_SAI, RGB_VAI, KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,   KC_NO,   KC_NO,    KC_NO,             KC_NO,    \
       KC_NO,   KC_NO,   RGB_HUD, RGB_SAD, RGB_VAD,KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,   KC_NO,   KC_NO,    F(1),    KC_NO,    KC_NO,    \
-      KC_NO,   KC_NO,   KC_NO,                            KC_NO,                           KC_NO,   KC_NO,    KC_NO,   KC_NO,    KC_NO),
+      KC_TRNS, KC_NO,   KC_NO,                            KC_NO,                           KC_NO,   KC_NO,    KC_NO,   KC_NO,    KC_NO),
 
 };
 
