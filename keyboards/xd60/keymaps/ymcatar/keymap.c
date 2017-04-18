e8847276de99c00c6640958e9ccb4204dd0f09b8
ymcatar <ymcatar@gmail.com>
2017-04-18 23:44:17 +0800

    update: keymap

--- keyboards/xd60/keymaps/ymcatar/keymap.c
+++ keyboards/xd60/keymaps/ymcatar/keymap.c
@@ -22,7 +22,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // 1: Function Layer
   KEYMAP(
       RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,  KC_F6,   KC_F7,  KC_F8,   KC_F9,    KC_F10,  KC_F11,  KC_F12,  KC_F13,   KC_F14,    \
-      KC_CAPS, KC_P7,   KC_P8,   KC_P9,   KC_NO,  KC_NO,  KC_NO,   KC_NO,  KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,             KC_DEL,    \
+      KC_CAPS, KC_P7,   KC_P8,   KC_P9,   KC_NO,  KC_NO,  KC_NO,   KC_NO,  KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_NO,             KC_BSPC,    \
       KC_NO,   KC_P4,   KC_P5,   KC_P6,   KC_NO,  KC_NO,  KC_LEFT, KC_DOWN,  KC_UP, KC_RIGHT, KC_NO,   KC_NO,   KC_NO,             KC_NO,    \
       KC_NO,   KC_NO,   KC_P1,   KC_P2,   KC_P3,  KC_NO,  KC_NO,   KC_NO,  KC_NO,   KC_NO,    KC_NO,   KC_NO,   KC_MPLY, KC_TRNS,  KC_INS,    \
       KC_TRNS, KC_NO,   KC_P0,                            KC_NO,                              KC_TRNS, KC_MPRV, KC_VOLD, KC_VOLU,  KC_MNXT),
